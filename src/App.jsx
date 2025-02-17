081e2ac86c278f3d5c4bf2f0c9f167c359dc00eb
pridav589 <pridav589@hengersor.hu>
2025-02-17 11:46:17 +0100

    Router beállítások
    browser router, és routok

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./oldalak/Home"
 import Ermesek from "./oldalak/Ermesek"
 import Mermesek from "./oldalak/Mermesek"
 import Navbar from "./Navbar"
-import {Route, Routes} from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import 'tachyons'
@@ -16,6 +16,11 @@ function App() {
         <h1>Covid</h1>
       </div>
 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/ermesek" element={<Ermesek />} />
+        <Route path="/mermesek" element={<Mermesek />} />
+      </Routes>
     </>
   )
 }
